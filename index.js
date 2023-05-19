a824252926955a7754a5a494a3ab5c5662b68431
ian841120 <82960534+ian841120@users.noreply.github.com>
2023-05-19 12:26:42 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+document.getElementById("follow").onclick=function()
+{
+    if(document.getElementById("follow").value=="追蹤")
+    {
+        document.getElementById("follow").value="已追蹤";
+       
+    }
+    else{
+        document.getElementById("follow").value="追蹤";
+    }
+    
+}
